bc5e35821880e881318f1ba79be2c1f3889ee7c1
xuqiu <xuqiu@smartncs.com>
2020-12-16 00:40:28 +0800

    防重复提交RepeatMethodInterceptor优化

--- starter/limiting-spring-boot-starter/src/main/java/com/github/aqiu202/limit/aop/RepeatMethodInterceptor.java
+++ starter/limiting-spring-boot-starter/src/main/java/com/github/aqiu202/limit/aop/RepeatMethodInterceptor.java
@@ -1,9 +1,9 @@
 package com.github.aqiu202.limit.aop;
 
 import com.github.aqiu202.aop.pointcut.AbstractKeyAnnotationInterceptor;
-import com.github.aqiu202.util.spel.EvaluationFiller;
 import com.github.aqiu202.limit.anno.RepeatLimiting;
 import com.github.aqiu202.lock.base.Lock;
+import com.github.aqiu202.util.spel.EvaluationFiller;
 import java.util.concurrent.TimeUnit;
 import org.aopalliance.intercept.MethodInvocation;
 
@@ -36,7 +36,8 @@ public class RepeatMethodInterceptor extends AbstractKeyAnnotationInterceptor<Re
     }
 
     @Override
-    protected void beforeIntercept(MethodInvocation invocation, RepeatLimiting repeatLimiting, String key) {
+    protected void beforeIntercept(MethodInvocation invocation, RepeatLimiting repeatLimiting,
+            String key) {
         long timeout = repeatLimiting.timeout();
         TimeUnit timeUnit = repeatLimiting.timeUnit();
         final Boolean getLock = this.lock.acquire(key, timeout, timeUnit);
@@ -46,10 +47,10 @@ public class RepeatMethodInterceptor extends AbstractKeyAnnotationInterceptor<Re
     }
 
     @Override
-    protected void afterIntercept(MethodInvocation invocation, RepeatLimiting repeatLimiting, String key, Throwable throwable) {
+    protected Object onError(MethodInvocation invocation, RepeatLimiting repeatLimiting, String key,
+            Throwable throwable) throws Throwable {
         //只有等到过期或者出现异常才释放锁，不会主动释放锁
-        if (throwable != null) {
-            this.lock.release(key, repeatLimiting.timeout(), repeatLimiting.timeUnit());
-        }
+        this.lock.release(key, repeatLimiting.timeout(), repeatLimiting.timeUnit());
+        throw throwable;
     }
 }
