2b466b4dd9fe30bf4127e1f02fd8379a1c2d68f7
aqiu <aqiuqiuweb@163.com>
2024-09-21 16:11:05 +0800

    组件优化

--- wechat/wechat-codec-spring-boot-starter/src/main/java/com/github/aqiu202/wechat/wxcodec/service/WxCodecServiceImpl.java
+++ wechat/wechat-codec-spring-boot-starter/src/main/java/com/github/aqiu202/wechat/wxcodec/service/WxCodecServiceImpl.java
@@ -9,17 +9,17 @@ import com.github.aqiu202.util.MD5Utils;
 import com.github.aqiu202.util.StringUtils;
 import com.github.aqiu202.wechat.wxcodec.bean.WxCodecProperty;
 import com.github.aqiu202.wechat.wxcodec.encoder.WxPKCS7Encoder;
-import java.io.IOException;
+import org.springframework.http.HttpEntity;
+import org.springframework.util.Assert;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
 public class WxCodecServiceImpl implements WxCodecService {
 
@@ -36,27 +36,20 @@ public class WxCodecServiceImpl implements WxCodecService {
 
     @Override
     public JsonNode login(String code) {
-        if (code == null || code.length() == 0) {
-            throw new IllegalArgumentException("登录凭证不能为空");
-        }
+        Assert.hasText(code, "登录凭证不能为空");
         // 授权（必填）
         String grant_type = "authorization_code";
         //////////////// 1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid
         // 请求参数
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid={1}&secret={2}&js_code={3}&grant_type={4}";
         // 发送请求
-        String str = restTemplate.getForObject(url, String.class,
+        JsonNode result = restTemplate.getForObject(url, JsonNode.class,
                 this.wxCodecProperty.getAppId(), this.wxCodecProperty.getSecret(), code,
                 grant_type);
         // 解析相应内容（转换成json对象）
-        if (StringUtils.hasText(str)) {
-            try {
-                return mapper.readTree(str);
-            } catch (IOException e) {
-                return null;
-            }
-        }
-        return null;
+        int errcode = result.get("errcode").asInt();
+        Assert.isTrue(errcode == 0, result.get("errmsg").asText());
+        return result;
     }
 
     @Override
@@ -81,7 +74,10 @@ public class WxCodecServiceImpl implements WxCodecService {
         headers.set("Content-Type", "application/json");
         HttpEntity<String> body = new HttpEntity<>(bodyString, headers);
         //{"errcode":0,"errmsg":"ok","phone_info":{"phoneNumber":"15764212419","purePhoneNumber":"15764212419","countryCode":"86","watermark":{"timestamp":1726847080,"appid":"wx1a94dcdb3490a778"}}}
-        return this.restTemplate.postForObject(url, body, JsonNode.class, accessToken);
+        JsonNode result = this.restTemplate.postForObject(url, body, JsonNode.class, accessToken);
+        int errcode = result.get("errcode").asInt();
+        Assert.isTrue(errcode == 0, result.get("errmsg").asText());
+        return result;
     }
 
     @Override
@@ -125,10 +121,11 @@ public class WxCodecServiceImpl implements WxCodecService {
 
     /**
      * 微信支付退款信息解密
-     * @author aqiu 2019/11/1 3:38 下午
+     *
      * @param content: 密文
-     * @param apiKey: 接口api秘钥
+     * @param apiKey:  接口api秘钥
      * @return {@link String}
+     * @author aqiu 2019/11/1 3:38 下午
      **/
     @Override
     public String decodeRefundInfo(String content, String apiKey) {
